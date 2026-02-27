d29343162f01e4f5fe23ec75e6618ae292bed402
AgopSagopyan <keremcemerturk@gmail.com>
2026-02-27 16:50:01 +0300

    Adminer.php update

---
+++ devenv.nix
@@ -0,0 +1,75 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/languages/
+  # languages.rust.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  # https://devenv.sh/basics/
+  enterShell = ''
+    hello         # Run scripts directly
+    git --version # Use packages
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+
+  languages.php = {
+    enable = true;
+  };
+
+  processes.php-server.exec = "php -S localhost:8000";
+
+  services.mysql = {
+    enable = true;
+    package = pkgs.mariadb;
+    initialDatabases = [{ name = "denemeDB"; }];
+
+    ensureUsers = [
+      {
+        name = "keremcem";
+        password = "kerem4567";
+        ensurePermissions = {
+          "denemeDB.*" = "ALL PRIVILEGES";
+        };
+      }
+      {
+        name = "backup";
+        ensurePermissions = {
+          "*.*" = "SELECT, LOCK TABLES";
+        };
+      }
+    ]
+    ;
+  };
+}
